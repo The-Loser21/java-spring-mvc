23eb46bdaf972e62fcb3f33c73a2c2c3ae7b78ae
The-Loser21 <lehai21092001@gmail.com>
2024-11-05 00:04:42 +0700

    config view engine

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,26 +7,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-
+    // DI : Dipendency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping(" ")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// // DI : Dipendency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping(" ")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
